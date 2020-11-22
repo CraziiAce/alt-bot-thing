643f1e8b2236b1ecdfd436032dad99dc38bec42b
CraziiAce <teddyjraz@gmail.com>
2020-11-22 14:43:58 -0700

    aaaaaaaaa

--- cogs/welcomer.py
+++ cogs/welcomer.py
@@ -27,13 +27,24 @@ class welcomer(commands.Cog):
                 self.data.insert_one({"_id": ctx.guild.id, "chnl": channel.id})
                 await ctx.send(f"Successfully set the welcome channel to {channel.mention}")
                 return
-        elif not channel and not doc['chnl']:
-            await ctx.send("You didn't specify a channel!")
-        elif not channel and doc['chnl'] and doc:
-            self.data.update_one(filter = {"_id": ctx.guild.id}, update={"$unset": {"chnl": ""}})
-            await ctx.send("Channel cleared")
-        elif channel and not doc['chnl']:
-            self.data.update_one
+            elif not channel:
+                await ctx.send("You didn't specify a channel!")
+                return
+        elif doc:
+            if channel and not doc['chnl']:
+                self.data.update_one(filter = {"_id": ctx.guild.id}, update={"$set": {"chnl": channel.id}})
+                await ctx.send(f"Successfully set the welcome channel to {channel.mention}")
+                return
+            elif not channel and not doc['chnl']:
+                await ctx.send("You didn't specify a channel!")
+                return
+            elif not channel and doc['chnl']:
+                self.data.update_one(filter = {"_id": ctx.guild.id}, update={"$unset": {"chnl": ""}})
+                await ctx.send("Channel cleared")
+                return
+        else:
+            await ctx.send(f"Sorry, but I encountered an unexpected error. Please contact support with `{ctx.prefix}supportrequest`")
+
     
     @welcomeset.command()
     async def joinmessage(self, ctx, msg: str = None):
