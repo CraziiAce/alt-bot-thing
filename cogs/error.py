26295fe1c6509acdc5d202655e443c1dc3f891bd
CraziiAce <teddyjraz@gmail.com>
2020-11-24 20:10:42 -0700

    and i forgot the $ operator

--- cogs/error.py
+++ cogs/error.py
@@ -109,7 +109,7 @@ class ErrorHandler(Cog):
                 self.data.insert_one({"id": "info"})
                 doc = self.data.find_one({"id": "info"})
             if not doc.get("numerror"):
-                self.data.update_one(filter={"id": "info"}, update={"set": {"numerror": 0}})
+                self.data.update_one(filter={"id": "info"}, update={"$set": {"numerror": 0}})
             await ctx.send(f"```\nThis command raised an error: {error}.\nError ID: {ctx.message.id}.```")
             self.data.insert_one({"id": doc.numerror + 1, "command": ctx.command, "fulltb": f"https://hastebin.com/{re['key']}"})
 
