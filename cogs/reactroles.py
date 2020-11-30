2053209d19e091ed58bd7f2a53626d4dc55f7169
CraziiAce <teddyjraz@gmail.com>
2020-11-30 14:08:58 -0700

    debug

--- cogs/reactroles.py
+++ cogs/reactroles.py
@@ -50,8 +50,8 @@ class reactionroles(commands.Cog):
                     guild = self.bot.get_guild(payload.guild_id)
                     role = guild.get_role(doc.get("role"))
                     await user.remove_roles(role, reason = "Reaction Roles")
-        except:
-            return
+        except Exception as e:
+            print(e)
 
 
 def setup(bot):
