88f1d8cb9a22273f98da18725bcb1c6e8a519f07
CraziiAce <teddyjraz@gmail.com>
2020-11-30 10:04:50 -0700

    debug

--- cogs/reactroles.py
+++ cogs/reactroles.py
@@ -34,7 +34,8 @@ class reactionroles(commands.Cog):
             print(payload.event_type)
             doc = self.data.find_one({"guildid": payload.guild_id, "msgid": payload.message_id, "chnlid": payload.channel_id})
             if payload.event_type == "REACTION_ADD":
-                print(f"{str(payload.emoji)}\n\n{payload.emoji.id}\n\n{doc.get['emoji']}")
+                print("firing")
+                print(f"{str(payload.emoji)}\n\n{payload.emoji.id}\n\n{doc['emoji']}")
                 print(f"{str(payload.emoji)}")
                 if str(payload.emoji) == doc.get("emoji") or payload.emoji.id == doc.get("emoji"):
                     print("firing")
