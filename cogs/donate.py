f7f5d7911fa1da73c855e7ed86233637f9a0c2d3
CraziiAce <teddyjraz@gmail.com>
2020-12-15 17:59:38 -0700

    what if we don't get donations

--- cogs/donate.py
+++ cogs/donate.py
@@ -32,21 +32,24 @@ class donate(commands.Cog):
     async def checkended(self):
         re = await requests.get("https://donatebot.io/api/v1/donations/718663089318527016/new", headers={"Authorization": self.token})
         r = await re.json()
-        for item in r['donations']:
-            g = self.bot.get_guild(718663089318527016)
-            mem = g.get_member(item['buyer_id'])
-            role = g.get_role(item['role_id'])
-            c = g.get_channel(788484736347144202)
-            if self.counter == 1:
-                await c.send("started")
-                self.counter += 1
-            await mem.add_roles(role, reason=f"Transaction id {item['txn_id']}")
-            time = datetime.datetime.utcfromtimestamp(item['timestamp'])
-            self.payments.insert_one({"_id": item['txn_id'], "status": item['status'], "buyer": {"email": item['buyer_email'], "id": item['buyer_id']}, "isSub": item['recurring'], "price": {"price": item['price'], "currency": item['currency']}, "time": time})
-            emb = discord.Embed(title="New donation!", color=color)
-            emb.add_field(name="Info", value=f"Transaction ID: {item['txn_id']}\nUse `e!donate info {item['txn_id']} for more!")
-            await c.send(embed=emb)
-            r = await requests.post(f"https://donatebot.io/api/v1/donations/718663089318527016/{item['txn_id']}/mark", headers={"Authorization": self.token})
+        try:
+            for item in r['donations']:
+                g = self.bot.get_guild(718663089318527016)
+                mem = g.get_member(item['buyer_id'])
+                role = g.get_role(item['role_id'])
+                c = g.get_channel(788484736347144202)
+                if self.counter == 1:
+                    await c.send("started")
+                    self.counter += 1
+                await mem.add_roles(role, reason=f"Transaction id {item['txn_id']}")
+                time = datetime.datetime.utcfromtimestamp(item['timestamp'])
+                self.payments.insert_one({"_id": item['txn_id'], "status": item['status'], "buyer": {"email": item['buyer_email'], "id": item['buyer_id']}, "isSub": item['recurring'], "price": {"price": item['price'], "currency": item['currency']}, "time": time})
+                emb = discord.Embed(title="New donation!", color=color)
+                emb.add_field(name="Info", value=f"Transaction ID: {item['txn_id']}\nUse `e!donate info {item['txn_id']} for more!")
+                await c.send(embed=emb)
+                r = await requests.post(f"https://donatebot.io/api/v1/donations/718663089318527016/{item['txn_id']}/mark", headers={"Authorization": self.token})
+        except:
+            await ctx.send("no donations :(")
         
 def setup(bot):
     bot.add_cog(donate(bot))
